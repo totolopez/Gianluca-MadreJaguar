7012ed25fcb7a0451e01c85b7de3bf23f87107b9
toto.lopez <alfonsolopezqa@gmail.com>
2023-09-22 21:13:01 -0300

    Inicio de JS

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,21 @@
+// Evento de los enlaces
+const enlaceGianluca = document.querySelector('.gianluca');
+const enlaceQuienessomos = document.querySelector('.quienessomos');
+const enlaceContacto = document.querySelector('.contacto'); 
+const enlaceClases = document.querySelector('.clases'); 
+
+enlaceGianluca = addEventListener('click', (event) => {
+    event.preventDefault();
+    const seccionQuiensoy = document.getElementById ('quiensoy');
+    seccionQuiensoy.scrollIntoView({behavior: 'smooth'});
+});
+
+
+enlaceQuienessomos = addEventListener('click', (event) => {
+    event.preventDefault();
+    const seccionQuienessomos = document.getElementById ('quienessomos');
+    seccionQuienessomos.scrollIntoView({behavior: 'smooth'});
+});
+
 
 
