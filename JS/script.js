f0092a49fb4246a5e09612841b858dc1213469bf
toto.lopez <alfonsolopezqa@gmail.com>
2023-09-23 18:08:50 -0300

    Probando CSS

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,53 @@
 // Evento de los enlaces
-const enlaceGianluca = document.querySelector('.gianluca');
-const enlaceQuienessomos = document.querySelector('.quienessomos');
+
+/* const enlaceGianluca = document.querySelector('.gian');
+const enlaceQuienessomos = documentquerySelector('#quienessomos');
 const enlaceContacto = document.querySelector('.contacto'); 
 const enlaceClases = document.querySelector('.clases'); 
 
-enlaceGianluca = addEventListener('click', (event) => {
-    event.preventDefault();
+enlaceGianluca = addEventListener('click', (event) => 
+{    event.preventDefault(;)
     const seccionQuiensoy = document.getElementById ('quiensoy');
-    seccionQuiensoy.scrollIntoView({behavior: 'smooth'});
+    seccionQuiensoy.scrollIntoView(b{ehavior: 'smooth'};)
 });
 
 
 enlaceQuienessomos = addEventListener('click', (event) => {
     event.preventDefault();
-    const seccionQuienessomos = document.getElementById ('quienessomos');
+    const seccionQuienessomos = document.getElementById ('quienessomos');   
     seccionQuienessomos.scrollIntoView({behavior: 'smooth'});
+}); */
+
+
+/* for (const enlace of enlaces) {
+    enlace.addEventListener('click', (event) =>{
+        event.preventDefault();
+        const seccion = document.getElementById(enlace.id);
+        seccion.scrollIntoView({behavior: 'smooth'})
+    })
+} */
+
+const enlacesNav = [
+    document.querySelector('#gianluca'),
+    document.querySelector('#quienessomos'),
+    document.querySelector('#contacto'),
+    document.querySelector('#clases') 
+]
+
+enlacesNav.forEach(enlace => {
+    enlace.addEventListener('click', (event) => {
+        event.preventDefault();
+        const seccion = document.getElementById(enlace.id)
+        seccion.scrollIntoView({behavior: 'smooth'})
+    })
 });
 
+const enlacesFooter = document.querySelectorAll ('.contacto a');
 
-
+enlacesFooter.forEach(enlace => {
+    enlace.addEventListener('click', (event) => {
+        event.preventDefault();
+        const url = enlace.getAttribute('href');
+        window.location = url;
+    });
+});
